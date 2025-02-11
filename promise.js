f08b5bc5954357b4ac4399e4d51e35947f7c64f2
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-02-11 23:10:13 +0530

    promise and callbackhell

---
+++ promise.js
@@ -0,0 +1,13 @@
+const github_api = "https://api.github.com/users/ambikamishra03";
+
+const user = fetch(github_api);
+console.log(user);
+
+// result - data returned
+// state - pending,rejected,fulfilled
+
+// promise is an object that represents eventual completion 
+// of asynchronous operation
+user.then((data) =>{
+ console.log(data);
+})
---
+++ task.js
@@ -0,0 +1,42 @@
+const user = [
+    {fname:"ambika",lname:"mishra",age:24},
+    {fname:"manish",lname:"agrahari",age:24},
+    {fname:"kittu",lname:"mishra",age:18},
+    {fname:"donald",lname:"trump",age:80},
+    {fname:"narendra",lname:"modi",age:80}
+]
+
+
+// fname and lname together
+
+const names = user.map((name)=>name.fname+" "+name.lname);
+console.log(names);
+
+// {24:2,18:1,80:1}
+
+const ages = user.reduce((acc,cur) =>{    
+ if(acc[cur.age]){
+    acc[cur.age]=++acc[cur.age];
+ }else{
+    acc[cur.age] =1; 
+ }
+ return acc;
+},{})
+console.log(ages);
+
+// fname of people age < 30
+const youngers = 
+// user.filter((i) => i.age<30)
+// .map((i) => i.fname)
+
+user.reduce((acc,cur)=>{
+  if(cur.age<30){
+    acc.push(cur.fname);
+  }
+  return acc;
+},[])
+
+console.log(youngers);
+
+
+
