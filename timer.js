e9d4b4cf0f9a599de995d68745dc35744e8b5757
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-03-16 21:41:27 +0530

    timers in js

---
+++ timer.js
@@ -0,0 +1,22 @@
+console.log("timers");
+
+// settimeout
+setTimeout(()=>{
+   console.log("setTimeout at 3 sec");
+},3000)
+
+let x=0;
+setInterval(()=>{
+ x++;
+ console.log(x);
+},3000);
+
+setInterval(()=>{
+ console.log("hello "+x++);
+},10000);
+
+// clearInterval(1); // will clear first timer
+clearInterval(2); //will clear second timer
+// clearTimeout(1); // will clear first timer
+// clearTimeout(2);  //will clear second timer
+// clearInterval(3); //will clear third timer
