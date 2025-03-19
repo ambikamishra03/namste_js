77cd7f073499e18de57aeee1412bd13ed89ab42f
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-03-19 17:31:39 +0530

    await keyword only used inside async function

--- async_await.js
+++ async_await.js
@@ -3,11 +3,19 @@
 const pr = new Promise((resolve, reject) =>{
       resolve("promise pr resolved")
 })
-async function getData() {
-    return pr;
-}
+// async function getData() {
+//     return pr;
+// }
 
-const data = getData();
-data.then((res)=>   console.log(res))
+// const data = getData();
+// data.then((res)=>   console.log(res))
 
 // console.log(data); 
+
+
+
+async function handlePromise() {
+    const data = await pr;
+    console.log(data);
+}
+handlePromise();
