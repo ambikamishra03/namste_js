53d673501fccbb3a33c87ef83686fc07b986c275
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-03-25 16:10:51 +0530

    loop based questions

--- practice questions/loop.js
+++ practice questions/loop.js
@@ -7,17 +7,75 @@ while(a<=10){
 }
 // console.log(sum);
 
-
-// for...of loop se string print kro
-let str="javascript";
-for (const i of str) {
-//    console.log(i); 
-}
-
 // print odd numbers
 
 for (let i = 1; i <= 20 ;i++) {
    if(i%2==1){
-     console.log(i);
+    //  console.log(i);
    }
 } 
+
+// factorial
+function fact(n) {
+  let result=1;
+  for(let i=1;i<=n;i++)
+  {
+    result=i*result;
+  }
+  return result;
+}
+// console.log(fact(5));
+
+// print grid of size n*n
+// 1 2 3 
+// 4 5 6
+// 7 8 9
+let num=1;
+
+function grid(n){
+  for(let i=0;i<n;i++){
+    let str="";
+      for(let j=0;j<n;j++){
+        str +=`${num} `
+         num++;
+      }
+      console.log(str);
+  }  
+}
+// grid(3);
+
+// reverse array using for loop
+let arr = [1,2,3,4,5,6];
+for(let i=0;i<Math.floor(arr.length/2);i++){
+  var temp=arr[i];
+  arr[i]=arr[arr.length-i-1];
+  arr[arr.length-i-1]=temp;
+}
+// console.log(arr);
+
+// while loop to log numbers from 1 to 100 div by 5 
+let i=1;
+while(i<100){
+  if(i%5==0) {
+  // console.log(i);
+  }
+i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
